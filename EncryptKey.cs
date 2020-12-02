4637ca0b254dde9c79ad47f5d955511851e7e8f2
Baptiste Chevallier <baptiste.chevallier@viacesi.fr>
2020-12-02 11:38:35 +0100

    V1 CryptoSoft

---
+++ EncryptKey.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace CryptoSoft
+{
+    class EncryptKey
+    {
+        public string key { get; set; }
+        private readonly string _appDataPath;
+        private readonly string _keyFilePath;
+        private readonly string _configPath;
+        /// <summary>
+        /// Object to get the key store in the config file
+        /// </summary>
+        public EncryptKey()
+        {
+            Environment.SetEnvironmentVariable("appDataPath",
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\CryptoSoft");
+            _appDataPath = Environment.GetEnvironmentVariable("appDataPath");
+            _keyFilePath = Environment.GetEnvironmentVariable("appDataPath") + @"\config\key.json";
+            _configPath = Environment.GetEnvironmentVariable("appDataPath") + @"\config";
+            //Console.WriteLine(_keyFilePath);
+            CheckAndPrepareEnvironment();
+            LoadConfig();
+        }
+        /// <summary>
+        /// Method to load the key from the config file
+        /// </summary>
+        private void LoadConfig()
+        {
+            CheckAndPrepareConfigKeyFile();
+            key = File.ReadAllText(_keyFilePath);
+        }
+        /// <summary>
+        /// Method to check if the key config file exist and is usable
+        /// </summary>
+        public void CheckAndPrepareConfigKeyFile()
+        {
+            // create file if doesn't exist
+            if (!File.Exists(_keyFilePath))
+            {
+                File.Create(_keyFilePath).Close();
+                //Console.WriteLine("file created");
+            }
+            // check file isn't empty
+            if (File.ReadAllText(_keyFilePath) == "")
+            {
+                File.WriteAllText(_keyFilePath, JsonSerializer.Serialize(key));
+                //Console.WriteLine("file write in");
+            }
+            else
+            {
+                // check if file is usable
+                try
+                {
+                    File.ReadAllText(_keyFilePath);
+                    //Console.WriteLine("file is usable");
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine(e);
+                    int i = 0;
+                    while (File.Exists(_configPath + @"\notReadableSavesFile_" + i + ".json"))
+                    {
+                        i++;
+                    }
+                    File.Move(_keyFilePath, _configPath + @"\notReadableSavesFile_" + i + ".json");
+                    //Console.WriteLine("new file + stock old file");
+                    CheckAndPrepareConfigKeyFile();
+                }
+            }
+        }
+        /// <summary>
+        /// Method to prepare the AppData environment
+        /// </summary>
+        public void CheckAndPrepareEnvironment()
+        {
+            if (Directory.Exists(_appDataPath))
+            {
+                CheckForOneDataType("key","config");
+            }
+            else
+            {
+                Directory.CreateDirectory(_appDataPath);
+                Directory.CreateDirectory(_appDataPath + @"\config");
+                File.Create(_appDataPath + @"\config\key.json").Close();
+            }
+        }
+        /// <summary>
+        /// Method to specify more config file to check and create if doesn't exist
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="directoryName"></param>
+        private void CheckForOneDataType(string fileName, string directoryName)
+        {
+            if (File.Exists(_appDataPath + @"\" + directoryName + @"\" + fileName + ".json")) return;
+            if (!Directory.Exists(_appDataPath + @"\" + fileName))
+            {
+                Directory.CreateDirectory(_appDataPath + @"\" + fileName);
+            }
+            File.Create(_appDataPath + @"\" + directoryName + @"\" + fileName + ".json").Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace CryptoSoft
+{
+    class Program
+    {
+        /// <summary>
+        /// CryptoSoft made by Chevallier Baptiste
+        /// To crypt file with XOR and an EncryptKey
+        /// to use the process : CryptoSooft.exe (sourceFile) (destinationFile)
+        /// </summary>
+        /// <param name="args">array of argument sourceFile and destinationFile</param>
+        static void Main(string[] args)
+        {
+            EncryptKey key = new EncryptKey();
+            Encrypt(args[0], key.key, args[1]);
+        }
+
+        /// <summary>
+        /// Method to encrypt the file
+        /// </summary>
+        /// <param name="sourceFile">The source file specified by the user </param>
+        /// <param name="key">The key to encrypt the data</param>
+        /// <param name="destinationFile">The destination file specified by the user </param>
+        static private void Encrypt(string sourceFile, string key, string destinationFile)
+        {
+            try
+            {   //Check if the destination file exist
+                CheckDestinationFile(destinationFile);
+                FileAttributes attr = File.GetAttributes(sourceFile);
+
+                //detect whether its a directory or file
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    throw new Exception("Source path is a folder");
+                }
+                else if (!File.Exists(sourceFile))
+                {
+                    throw new Exception("Source path doesn't exist");
+                }
+                else
+                {
+                    string content = File.ReadAllText(sourceFile);
+                    
+                    StringBuilder szInputStringBuild = new StringBuilder(content);
+                    StringBuilder szOutStringBuild = new StringBuilder(content.Length);
+                    int keyLen = key.Length;
+                    char Textch;
+                    for (int iCount = 0; iCount < content.Length; iCount++)
+                    {
+                        Textch = szInputStringBuild[iCount];
+                        Textch = (char)(Textch ^ key[iCount % keyLen]);
+                        szOutStringBuild.Append(Textch);
+                    }
+                    File.WriteAllText(destinationFile, szOutStringBuild.ToString());
+                    //Console.WriteLine("Encrypt Done !");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        /// <summary>
+        /// method to check and then create the file if doesn't exist
+        /// </summary>
+        /// <param name="destinationFile"></param>
+        private static void CheckDestinationFile(string destinationFile)
+        {
+            //Create Destination repo if doesn't exist
+            if (File.Exists(destinationFile) == false)
+            {
+                File.Create(destinationFile).Close();
+            }
+        }
+    }
+}
